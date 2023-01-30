4a3b1c8ba0027914ada6fb5267741a421cb79319
Samantha Zitzelsberger <moonlighticewolf@gmail.com>
2023-01-30 13:14:18 -0700

    Added weapons class

--- Class work unit 1 pratice.cs
+++ Class work unit 1 pratice.cs
@@ -3,12 +3,18 @@ using System;
 public class Program
 {
 	public Weapon miniGun;
+	public PowerUp weaponPowerUp;
 		
 	public void Main()
 	{
 		miniGun = new Weapon(); 
-		Console.WriteLine(miniGun.weaponName +" has an ammo count of " + miniGun.ammoCount);
+		weaponPowerUp = new PowerUp();
 		
+		miniGun.ammoCount ++;
+		miniGun.firepower += weaponPowerUp.powerLevel;
+		Console.WriteLine(miniGun.weaponName + " has an ammo count of " + miniGun.ammoCount + ".");
+		Console.WriteLine(" The fire power of is now " + miniGun.firepower + " has increased." );
+	
 	}
 }
 
@@ -16,4 +22,8 @@ public class Weapon {
 	public int ammoCount = 10;
 	public string weaponName = "Mini Gun";
 	public float firepower = 11.5f;
+}
+
+public class PowerUp {
+	public float powerLevel = 2.5f;
 }
