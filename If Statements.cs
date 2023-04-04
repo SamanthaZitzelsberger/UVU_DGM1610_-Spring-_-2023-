064db2b95a3660aa63e3b6f7c09b17c1d5a3aaf1
Samantha Zitzelsberger <moonlighticewolf@gmail.com>
2023-04-04 15:01:31 -0600

    doing the if statements lab

---
+++ If Statements.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Program 
+{
+    public void Main()
+    {
+        Console.WrtiteLine("Welcome");
+        DoMath(10, 4);
+        DoMath(20, 7);
+        DoMath(30, 15);
+        Compare(4, 3);
+        Compare(3, 4);
+    }
+
+    public void DoMath (int value, int value2) {
+        var number = value + value2;
+        Console.WriteLine(number);
+    }
+
+    public void Compare (int value, int value2) {
+        if(value > value2) {
+            Console.WriteLine("True, the first is greater.");
+        } else {
+            COnsole.WriteLine("False, the second is greater.");
+        }
+    }
+}
