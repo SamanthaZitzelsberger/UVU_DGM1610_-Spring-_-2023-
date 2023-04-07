9250760d0bc5fdeecc7366433abb2ee486c44a82
Samantha Zitzelsberger <moonlighticewolf@gmail.com>
2023-04-07 14:57:22 -0600

    finished pratice

--- lab 1b/If Statements pratice.cs
+++ lab 1b/If Statements pratice.cs
@@ -5,7 +5,21 @@ public class Program
     public void Main()
     {   
         if (20 > 18) {
-            Console
+            Console.WriteLine("20 is greater than 18");
+        }
+
+        int x = 20;
+        int y = 18;
+        if (x > y) {
+            Console.WriteLine("x is greater than y");
+        }
+
+        int time = 20;
+        if (time < 18) {
+            Console.WriteLine("Good Day.");
+        }
+        else {
+            Console.WriteLine("Good evening.");
         }
     }
 }
