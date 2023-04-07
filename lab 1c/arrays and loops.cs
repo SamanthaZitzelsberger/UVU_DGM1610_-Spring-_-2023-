30bb48fcb857eb01e5e888f5e2af0669b22ecd5c
Samantha Zitzelsberger <moonlighticewolf@gmail.com>
2023-04-07 12:26:00 -0600

    watched first video

---
+++ lab 1c/arrays and loops.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Program 
+{
+    public void Main()
+    {
+        string[] cars = {"Mustang", "Camero", "HellCat"};
+        Console.WriteLine(cars.Length);
+        Console.WriteLine(cars[2]);
+    }
+}
