b0c10f500896543f3f8ed7a185d25d6026578967
Samantha Zitzelsberger <moonlighticewolf@gmail.com>
2023-04-07 11:42:03 -0600

    Added my operations

--- If Statements.cs
+++ If Statements.cs
@@ -2,19 +2,23 @@ using System;
 
 public class Program 
 {
+    public Operations myOperator;
     public void Main()
     {
-        Console.WrtiteLine("Welcome");
-        DoMath(10, 4);
-        DoMath(20, 7);
-        DoMath(30, 15);
-        Compare(4, 3);
-        Compare(3, 4);
-        CheckPassword ("SevenOF9");
-        CheckPassword ("OU812");
-    }
+        myOperator = new Operations();
 
-    public void DoMath (int value, int value2) {
+        Console.WrtiteLine("Welcome");
+        myOperator.DoMath(10, 4);
+        myOperator.DoMath(20, 7);
+        myOperator.DoMath(30, 15);
+        myOperator.Compare(4, 3);
+        myOperator.Compare(3, 4);
+        myOperator.CheckPassword ("SevenOF9");
+        myOperator.CheckPassword ("OU812");
+    }
+}
+public class Operations {
+     public void DoMath (int value, int value2) {
         var number = value + value2;
         Console.WriteLine(number);
     }
