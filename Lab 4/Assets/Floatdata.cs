e0f97a53aaef2230fcf0a15ace0ae08de5fd0c5b
Samantha Zitzelsberger <Moonlighticewolf@gmail.com>
2023-04-12 10:52:19 -0600

    did the health system

---
+++ Lab 4/Assets/Floatdata.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Floatdata : ScriptableObject
+{
+   public float value;
+
+   public void UpdateValue(float num)
+   {
+      value += num;
+   }
+}
