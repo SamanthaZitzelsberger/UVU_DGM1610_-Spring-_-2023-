36cdcf8ca40684461643c8fb1ba3d6fa38a0ceb6
Samantha Zitzelsberger <Moonlighticewolf@gmail.com>
2023-04-15 12:19:05 -0600

    changed some things

--- lab 4 new/Assets/KeyDownEvent.cs
+++ lab 4 new/Assets/KeyDownEvent.cs
@@ -13,19 +13,11 @@ public class KeyboardEventTest : MonoBehaviour
         root.Add(new TextField());
         root.Q<TextField>().Focus();
         root.RegisterCallback<KeyDownEvent>(OnKeyDown, TrickleDown.TrickleDown);
-        root.RegisterCallback<KeyUpEvent>(OnKeyUp, TrickleDown.TrickleDown);
     }
     void OnKeyDown(KeyDownEvent ev)
     {
         Debug.Log("KeyDown:" + ev.keyCode);
         Debug.Log("KeyDown:" + ev.character);
         Debug.Log("KeyDown:" + ev.modifiers);
-    }
-
-    void OnKeyUp(KeyUpEvent ev)
-    {
-        Debug.Log("KeyUp:" + ev.keyCode);
-        Debug.Log("KeyUp:" + ev.character);
-        Debug.Log("KeyUp:" + ev.modifiers);
     }
 }
