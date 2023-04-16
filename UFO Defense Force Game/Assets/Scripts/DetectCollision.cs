f9d17e5f055165a6a98e855472f91142cf863036
Samantha Zitzelsberger <Moonlighticewolf@gmail.com>
2023-04-16 12:49:42 -0600

    6 c part 2

--- UFO Defense Force Game/Assets/Scripts/DetectCollision.cs
+++ UFO Defense Force Game/Assets/Scripts/DetectCollision.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class DetectCollision : MonoBehaviour
 {
+    public ScoreManager scoreManager;
+
+    void Start()
+    {
+        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+    }
+
+    public int scoreToGive;
     void OnTriggerEnter(Collider other)
     {
+        scoreManager.IncreaseScore(scoreToGive);
         Destroy(gameObject);
         Destroy(other.gameObject);
     }
