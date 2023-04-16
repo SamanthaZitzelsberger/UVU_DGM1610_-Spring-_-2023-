e82fef6060cc3ae08c30112418251b0bc3e059c3
Samantha Zitzelsberger <Moonlighticewolf@gmail.com>
2023-04-15 21:10:37 -0600

    working on pickup power up

---
+++ UFO Defense Force Game/Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvent : MonoBehaviour
+{
+    public UnityEvent triggerEnterEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+        Destroy(this);
+    }
+}
