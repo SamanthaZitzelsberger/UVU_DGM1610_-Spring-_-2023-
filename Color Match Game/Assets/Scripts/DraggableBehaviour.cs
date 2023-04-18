34c78d79d616d431ebd4c306304973bbcfd2cead
Samantha Zitzelsberger <Moonlighticewolf@gmail.com>
2023-04-18 01:58:44 -0600

    worked on 7b part 3

--- Color Match Game/Assets/Scripts/DraggableBehaviour.cs
+++ Color Match Game/Assets/Scripts/DraggableBehaviour.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DraggableBehaviour : MonoBehaviour
 {
     public Camera cameraObj;
     public bool draggable;
-    public Vector3 position;
+    public Vector3 position, offset;
+    public UnityEvent startDragEvent, endDragEvent;
+
     void Start()
     {
         cameraObj = Camera.main;
@@ -15,18 +18,23 @@ public class DraggableBehaviour : MonoBehaviour
 
     public IEnumerator OnMouseDown()
     {
+        offset = transform.position - cameraObj.ScreenToWorldPoint(Input.mousePosition);
         draggable = true;
+        startDragEvent.Invoke();
+        yield return new WaitForFixedUpdate();
+    
 
         while(draggable)
         {
             yield return new WaitForFixedUpdate();
-            position = cameraObj.ScreenToViewportPoint(Input.mousePosition);
+            position = cameraObj.ScreenToWorldPoint(Input.mousePosition) + offset;
             transform.position = position;
-            Debug.Log("Drag");
+            //Debug.Log("Drag");
         }
     }
     private void OnMouseUp()
     {
         draggable = false;
+        endDragEvent.Invoke();
     }
 }
