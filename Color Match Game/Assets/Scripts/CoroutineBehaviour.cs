3b28990c87f89329a7240dc67976c6a8c9b27b98
Samantha Zitzelsberger <Moonlighticewolf@gmail.com>
2023-04-19 02:01:56 -0600

    working on 7b part 1

--- Color Match Game/Assets/Scripts/CoroutineBehaviour.cs
+++ Color Match Game/Assets/Scripts/CoroutineBehaviour.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CoroutineBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public UnityEvent repeatEvent;
 
-    // Update is called once per frame
-    void Update()
+    public int counterNum = 3;
+    public float seconds = 3.0f;
+    private WaitForSeconds wfsObj;
+    private WaitForFixedUpdate wffuObj;
+
+    IEnumerator Start()
     {
-        
+        wfsObj = new WaitForSeconds(seconds);
+        wffuObj = new WaitForFixedUpdate();
+
+        while (counterNum > 0)
+        {
+            yield return wfsObj;
+        }
     }
 }
