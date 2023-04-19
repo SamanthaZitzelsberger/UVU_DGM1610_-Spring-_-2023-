99e3af9731ac8bdd2646527ec166f363f2d8d076
Samantha Zitzelsberger <Moonlighticewolf@gmail.com>
2023-04-19 12:15:39 -0600

    started on 7b part 2

---
+++ Color Match Game/Assets/Scripts/Instancer.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Instancer : ScriptableObject
+{
+    public void CreateInstance(GameObject obj)
+    {
+        Instantiate(obj);
+    }
+}
